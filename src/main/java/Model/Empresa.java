35cf359ef5f496d83691aafe82d2bdb70e788d56
DESKTOP-3AEEJ23\Matias <matiasdp2003@gmail.com>
2021-09-02 19:23:09 -0300

    Ahora si

--- src/main/java/Model/Empresa.java
+++ src/main/java/Model/Empresa.java
@@ -2,4 +2,66 @@ package Model;
 
 public class Empresa {
 
+	public String NombreEmpresa;
+	public String Sucursal;
+	public int CodigoPostal;
+	public String Colonia;
+	public String Calle;
+	public int NumDomicilio;
+	public int NumDomPart;
+	public String Municipio;
+	public String Ciudad;
+	public String Provincia;
+	public int Telefono;
+	public String PaginaWeb;
+	
+	public void setNombreEmpresa(String NombreEmpresa) {
+		this.NombreEmpresa = NombreEmpresa;
+	}
+	public void setSucursal(String Sucursal) {
+		this.Sucursal = Sucursal;
+	}
+	public void setCodigoPostal(int CodigoPostal) {
+		this.CodigoPostal = CodigoPostal;
+	}
+	public void setColonia(String Colonia) {
+		this.Colonia = Colonia;
+	}
+	public void setCalle(String Calle) {
+		this.Calle = Calle;
+	}
+	public void setNumDomicilio(int NumDomicilio) {
+		this.NumDomicilio = NumDomicilio;
+	}
+	public void setNumDomPart(int NumDomPart) {
+		this.NumDomPart = NumDomPart;
+	}
+	public void setMunicipio(String Municipio) {
+		this.Municipio = Municipio;
+	}
+	public void setCiudad(String Ciudad) {
+		this.Ciudad = Ciudad;
+	}
+	public void setProvincia(String Provincia) {
+		this.Provincia = Provincia;
+	}
+	public void setTelefono(int Telefono) {
+		this.Telefono = Telefono;
+	}
+	public void setPaginaWeb(String PaginaWeb) {
+		this.PaginaWeb = PaginaWeb;
+	}
+	public String getNombreEmpresa(){return NombreEmpresa;}
+	public String getSucursal(){return Sucursal;}
+	public int getCodigoPostal(){return CodigoPostal;}
+	public String getColonia(){return Colonia;}
+	public String getCalle(){return Calle;}
+	public int getNumDomicilio(){return NumDomicilio;}
+	public int getNumDomPart(){return NumDomPart;}
+	public String getMunicipio(){return Municipio;}
+	public String getCiudad(){return Ciudad;}
+	public String getProvincia(){return Provincia;}
+	public int getTelefono(){return Telefono;}
+	public String getPaginaWeb(){return PaginaWeb;}
+	
 }
--- src/main/java/Model/Usuario.java
+++ src/main/java/Model/Usuario.java
@@ -2,4 +2,22 @@ package Model;
 
 public class Usuario {
 
+	public String mail;
+    public String contra;
+    public String tipo;
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+    public String getMail(){return mail;}
+
+    public void setContra(String contra) {
+        this.contra = contra;
+    }
+    public String getContra(){return contra;}
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+    public String getTipo(){return tipo;}
 }
